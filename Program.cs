c33a3ad88a3d017b70984f1cd032625a6a2c8ded
Austin Webre <austin.webre@gmail.com>
2020-09-26 00:00:46 -0500

    initial commit - gui.cs set up and basic window

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Terminal.Gui;
+
+namespace SillyTextAdventure
+{
+    class Program
+    {
+
+        static bool Quit()
+        {
+            var answer = MessageBox.Query(50, 7, "Quit Game", "Are you sure you want to quit the game?", "Yes", "No");
+            return answer == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Application.Init();
+            var top = Application.Top;
+            var topFrame = top.Frame;
+
+            var window = new Window("Hello")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill() - 1
+            };
+
+            var menuBar = new MenuBar(new MenuBarItem[]{
+                new MenuBarItem ("_Silly Text Adventure", new MenuItem[]{
+                    new MenuItem("_Quit", "", () => {if(Quit()){
+                        top.Running = false;
+                    }})
+                })
+            });
+
+            top.Add(window, menuBar);
+            top.Add(menuBar);
+            Application.Run();
+        }
+    }
+}
